50a4ae9172d3e3426db21223c90f4111de906daf
depressedX <yuri8023@qq.com>
2018-01-30 05:41:35 +0000

    Update motorHandler.py

--- motorHandler.py
+++ motorHandler.py
@@ -1,30 +1,32 @@
 # coding:utf-8
 import time
-import GPIO as GPIO
+import RPi.GPIO as GPIO
 
 # 上电机脉冲针脚
 UP_PULSE_PIN = 40
 
 # 下电机脉冲针脚
-DOWN_PULSE_PIN = 40
+DOWN_PULSE_PIN = 38
 
 # 进卸球脉冲\方向针脚
-LOAD_PULSE_PIN = 40
-LOAD_DIRECTION_PIN = 40
+LOAD_PULSE_PIN = 37
+LOAD_DIRECTION_PIN = 36
 
 # 步进电机脉冲\方向针脚
-PWM_PULSE_PIN = 40
-PWM_DIRECTION_PIN = 40
+PWM_PULSE_PIN = 33
+PWM_DIRECTION_PIN = 32
 
 # 步进电机最大旋转角度
-MAX_ROTATION_THETA = 40
+MAX_ROTATION_THETA = 75
 
 # 步进电机单步移动角度
 SINGLE_STEP_ANGLE = 1.8 / 4
 # 步进电机当前角度
 curAngle = 0
 # 步进电机移动速度因子
-pwmSpeedFactor = .05
+pwmSpeedFactor = .01
+# 脉冲持续时间
+pwmPulseFactor = .01
 
 upMotor = None
 downMotor = None
@@ -49,20 +51,27 @@ def pulse(channel, long):
 # PWM电机旋转角度[-max, max]
 def movePWM(angle):
     global curAngle
+    
+    if angle > MAX_ROTATION_THETA or angle < -MAX_ROTATION_THETA:
+    	print('out of bounds!')
+    	return
 
     delta = angle - curAngle
     if delta > 0:
         # 顺时针旋转
         GPIO.output(PWM_DIRECTION_PIN, GPIO.LOW)
-        delta = -delta
     else:
         # 逆时针旋转
         GPIO.output(PWM_DIRECTION_PIN, GPIO.HIGH)
+        delta = -delta
+    time.sleep(.05)
     steps = int(delta / SINGLE_STEP_ANGLE)
     for i in range(steps):
-        pulse(PWM_PULSE_PIN, .05)
+        pulse(PWM_PULSE_PIN, pwmPulseFactor)
         time.sleep(pwmSpeedFactor)
     curAngle = steps * SINGLE_STEP_ANGLE
+    
+    print('moveto ',angle,' now ',curAngle)
 
 
 # 修改上旋电机速度 [0,1]
@@ -94,20 +103,29 @@ def stopLoadMotor():
 def startUnloadBalls():
     pass
 
+def init():
+    #global upMotor, downMotor, loadMotorFre
+    try:
+	GPIO.setmode(GPIO.BOARD)
+ 	GPIO.setup(UP_PULSE_PIN, GPIO.OUT)
+    	GPIO.setup(DOWN_PULSE_PIN, GPIO.OUT)
+    	GPIO.setup(LOAD_PULSE_PIN, GPIO.OUT)
+        GPIO.setup(LOAD_DIRECTION_PIN, GPIO.OUT)
+        GPIO.setup(PWM_PULSE_PIN, GPIO.OUT)
+        GPIO.setup(PWM_DIRECTION_PIN, GPIO.OUT)
 
-try:
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(UP_PULSE_PIN, GPIO.OUT)
-    GPIO.setup(DOWN_PULSE_PIN, GPIO.OUT)
-    GPIO.setup(LOAD_PULSE_PIN, GPIO.OUT)
-    GPIO.setup(LOAD_DIRECTION_PIN, GPIO.OUT)
-    GPIO.setup(PWM_PULSE_PIN, GPIO.OUT)
-    GPIO.setup(PWM_DIRECTION_PIN, GPIO.OUT)
-
-    upMotor = GPIO.PWM(UP_PULSE_PIN, upMotorFrequency)
-    downMotor = GPIO.PWM(DOWN_PULSE_PIN, downMotorFrequency)
-    loadMotorFrequency = GPIO.PWM(LOAD_PULSE_PIN, loadMotorFrequency)
-except RuntimeError:
-    print(RuntimeError)
-finally:
+        upMotor = GPIO.PWM(UP_PULSE_PIN, upMotorFrequency)
+        downMotor = GPIO.PWM(DOWN_PULSE_PIN, downMotorFrequency)
+        loadMotor = GPIO.PWM(LOAD_PULSE_PIN, loadMotorFrequency)
+    except SyntaxError:
+        print('初始化失败 重新执行')
+        #GPIO.cleanup()
+        print(Exception)
+        
+        
+def destroy():
     GPIO.cleanup()
+
+
+if __name__ == '__main__':
+    print('run main')
