d63609746c833cfbe4314ef8430ab444892a4670
lph <yuri8023@qq.com>
2018-02-13 13:45:54 +0800

    .,..

--- serialDriver.py
+++ serialDriver.py
@@ -1,2 +1,54 @@
 # coding:utf-8
 # 树莓派通过串口从TX2接受电机控制信号
+import serial
+import json
+
+
+def handleSignal(signal):
+    signal_type = signal['type']
+    if signal_type == 'start':
+        print('start')
+    elif signal_type == 'shutdown':
+        print('shutdown')
+    elif signal_type == 'load':
+        payload = signal['payload']
+        if payload == 'load':
+            print('start load balls')
+        elif payload == 'unload':
+            print('start unload balls')
+        elif payload == 'pause':
+            print('pause load motor')
+        else:
+            print('wrong action {}'.format(payload))
+    elif signal_type == 'position':
+        pos = signal['payload']
+        if type(pos) == int and 0 <= pos <= 9:
+            print('move to position {}'.format(pos))
+        else:
+            print('wrong position {}'.format(pos))
+    else:
+        print('wrong signal type {}'.format(signal_type))
+
+
+def listen():
+    # 波特率
+    baudrate = 19200
+
+    ser = serial.Serial('/dev/ttyAMA0', baudrate)
+    print('listening on {}'.format(ser.name))
+
+    buffer_string = bytes()
+    try:
+        while True:
+            x = ser.read(1)
+            if str(x) == '\0':
+                # 已经结束
+                handleSignal(json.loads(buffer_string))
+                print('receive data: {}'.format(buffer_string))
+                buffer_string = bytes()
+            else:
+                buffer_string = buffer_string + x
+
+    finally:
+        ser.close()
+
--- serialTransmitter.py
+++ serialTransmitter.py
@@ -1,2 +1,80 @@
 # coding:utf-8
 # TX2用于通过串口向树莓派发送电机控制指令程序
+import serial
+import json
+
+# 波特率
+baudrate = 19200
+
+ser = serial.Serial()
+ser.baudrate = baudrate
+ser.port = '/dev/ttyTHS2'
+
+
+def init():
+    global ser
+
+    ser.open()
+    print('using {}'.format(ser.name))
+
+
+def start():
+    jsonObject = {
+        'type': 'start'
+    }
+    print('start')
+    ser.write(bytes(json.dumps(jsonObject) + '\0'))
+
+
+def shutdown():
+    jsonObject = {
+        'type': 'shutdown'
+    }
+    print('shutdown')
+    ser.write(bytes(json.dumps(jsonObject) + '\0'))
+
+
+def loadBalls():
+    jsonObject = {
+        'type': 'load',
+        'payload': 'load'
+    }
+    print('start load balls')
+    ser.write(bytes(json.dumps(jsonObject) + '\0'))
+
+
+def unloadBalls():
+    jsonObject = {
+        'type': 'load',
+        'payload': 'unload'
+    }
+    print('start unload balls')
+    ser.write(bytes(json.dumps(jsonObject) + '\0'))
+
+
+def stopLoadMotor():
+    jsonObject = {
+        'type': 'load',
+        'payload': 'pause'
+    }
+    print('stop load motor')
+    ser.write(bytes(json.dumps(jsonObject) + '\0'))
+
+
+def move(pos):
+    if not (type(pos) == int and 0 <= pos <= 9):
+        print('wrong parameter : position {}'.format(pos))
+        return
+    jsonObject = {
+        'type': 'position',
+        'payload': pos
+    }
+    print('move to position {}'.format(pos))
+    ser.write(bytes(json.dumps(jsonObject) + '\0'))
+
+
+# 必须结束程序前调用
+def destroy():
+    global ser
+    if ser.is_open:
+        ser.close()
